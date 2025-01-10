640559960434dc481b7b9cc6810c8b8003009243
Kuldyaev <slavalion1@gmail.com>
2025-01-10 04:52:30 +0200

    catalog page addded

--- src/pages/CatalogPage/index.tsx
+++ src/pages/CatalogPage/index.tsx
@@ -1,5 +1,17 @@
+import { SearchIcon } from "../../features/Icons";
+
+import s from "./style.module.scss";
+
 const CatalogPage = () => {
-  return <div>CatalogPage</div>;
+  return (
+    <div className={s.catalogDesk}>
+      <h3>Каталог</h3>
+      <div className={s.searchBar}>
+        <SearchIcon />
+        <h6 className={s.searchInput}>Товар, бренд или артикул</h6>
+      </div>
+    </div>
+  );
 };
 
 export default CatalogPage;
