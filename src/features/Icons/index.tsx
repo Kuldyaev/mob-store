20a79a34abb91184158d43b7560f751331723b6c
Kuldyaev <slavalion1@gmail.com>
2025-01-12 23:53:28 +0200

    Profile with menu

--- src/features/Icons/index.tsx
+++ src/features/Icons/index.tsx
@@ -3,5 +3,19 @@ import ProfileIcon from "./ProfileIcon";
 import CatalogIcon from "./CatalogIcon";
 import MainPageIcon from "./MainPageIcon";
 import CartIcon from "./CartIcon";
+import ArrowToRight from "./ArrowToRight";
+import DeliveriesIcon from "./DeliveriesIcon";
+import StarIcon from "./StarIcon";
+import SubscribesIcon from "./SubscribesIcon";
 
-export { SearchIcon, ProfileIcon, CatalogIcon, MainPageIcon, CartIcon };
+export {
+  SearchIcon,
+  ProfileIcon,
+  CatalogIcon,
+  MainPageIcon,
+  CartIcon,
+  ArrowToRight,
+  DeliveriesIcon,
+  StarIcon,
+  SubscribesIcon,
+};
--- src/pages/ProfilePage/index.tsx
+++ src/pages/ProfilePage/index.tsx
@@ -1,5 +1,19 @@
 import ProfileTopAva from "../../components/ProfileTopAva";
+import {
+  ArrowToRight,
+  DeliveriesIcon,
+  StarIcon,
+  SubscribesIcon,
+} from "../../features/Icons";
 import s from "./style.module.scss";
+
+const menu = [
+  {
+    id: 0,
+    title: "Доставки",
+    info: "Ближайшие не ожидаются",
+  },
+];
 
 const ProfilePage = () => {
   return (
@@ -32,7 +46,58 @@ const ProfilePage = () => {
           </div>
         </div>
       </div>
-      <div className={s.profileDeskMenu}>bdf</div>
+      <div className={s.profileDeskMenu}>
+        <div className={s.profileDeskMenuItem}>
+          <div className={s.profileDeskMenuIcon}>
+            <div className={s.profileDeskMenuIconPlace}>
+              <DeliveriesIcon />
+            </div>
+          </div>
+          <div className={s.profileDeskMenuInfo}>
+            <div className={s.profileDeskMenuText}>
+              <h4>Доставки</h4>
+              <h5>Ближайшие не ожидаются</h5>
+            </div>
+            <button>
+              <ArrowToRight />
+            </button>
+          </div>
+        </div>
+
+        <div className={s.profileDeskMenuItem}>
+          <div className={s.profileDeskMenuIcon}>
+            <div className={s.profileDeskMenuIconPlace}>
+              <StarIcon />
+            </div>
+          </div>
+          <div className={s.profileDeskMenuInfo}>
+            <div className={s.profileDeskMenuText}>
+              <h4>Покупки</h4>
+              <h5>Здесь можно купить что-то заново</h5>
+            </div>
+            <button>
+              <ArrowToRight />
+            </button>
+          </div>
+        </div>
+
+        <div className={s.profileDeskMenuItem}>
+          <div className={s.profileDeskMenuIcon}>
+            <div className={s.profileDeskMenuIconPlace}>
+              <SubscribesIcon />
+            </div>
+          </div>
+          <div className={s.profileDeskMenuInfo}>
+            <div className={s.profileDeskMenuText}>
+              <h4>Подписки на бренды</h4>
+              <h5>Nike, Adidas, New Balance, Puma</h5>
+            </div>
+            <button>
+              <ArrowToRight />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
