67aa0769b5ec7b966fee5fa431ffd6a0de30c961
CosmoStarPL <cosmostarpl@gmail.com>
2025-01-14 22:19:53 +0500

    Adding CardPage

--- src/pages/CartPage/index.tsx
+++ src/pages/CartPage/index.tsx
@@ -1,5 +1,21 @@
+import CartItem from "../../components/CarItem";
+
 const CartPage = () => {
-  return <div>CartPage</div>;
+  const goods = [
+    {
+      id: 1,
+      type: "shoes",
+      img: "nike",
+      desc: "Кроссовки The Powerpuff Girls x Nike SB Dunk Low «Buttercup»",
+      size: "43 EU",
+      delivery: "5-10",
+      color: "Зеленый",
+      price: "24 690",
+    }
+  ]
+  return <div>
+    <CartItem goods={goods}/>
+  </div>;
 };
 
 export default CartPage;
