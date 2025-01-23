40680a79a1acd63f9dcaaffa65b6c0579ce86d55
Kuldyaev <slavalion1@gmail.com>
2025-01-23 13:54:28 +0200

    headerAnimation for cartPage

--- src/features/Icons/TrashCanIcon.tsx
+++ src/features/Icons/TrashCanIcon.tsx
@@ -10,37 +10,37 @@ const TrashCanIcon = () => {
       <path
         d="M17.5 4.98478C14.725 4.70978 11.9333 4.56812 9.15 4.56812C7.5 4.56812 5.85 4.65145 4.2 4.81812L2.5 4.98478"
         stroke="#7E7F83"
-        stroke-width="1.8"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.8"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M7.08398 4.14102L7.26732 3.04935C7.40065 2.25768 7.50065 1.66602 8.90898 1.66602H11.0923C12.5007 1.66602 12.609 2.29102 12.734 3.05768L12.9173 4.14102"
         stroke="#7E7F83"
-        stroke-width="1.8"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.8"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M16 8L15.4307 15.8305C15.3343 17.0513 15.2555 18 12.8117 18H7.18832C4.74453 18 4.66569 17.0513 4.56934 15.8305L4 8"
         stroke="#7E7F83"
-        stroke-width="1.8"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.8"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M8.60938 13.75H11.3844"
         stroke="#7E7F83"
-        stroke-width="1.8"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.8"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M7.91602 10.416H12.0827"
         stroke="#7E7F83"
-        stroke-width="1.8"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.8"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
